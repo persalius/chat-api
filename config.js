632a426547011ba2935da406b14ebc523bed57f6
Sergey <persalius@gmail.com>
2019-12-20 21:27:49 +0200

    edit

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  JWT_SECRET: process.env.JWT_SECRET || 'dogecodes-secret',
-  MONGODB_URI:
-    process.env.MONGODB_URI ||
-    'mongodb://sergey90:sergey90@ds257668.mlab.com:57668/chat',
+  JWT_SECRET: process.env.JWT_SECRET || 'persalius-secret',
+  MONGODB_URI: 'mongodb://sergey90:sergey90@ds257668.mlab.com:57668/chat',
 };
