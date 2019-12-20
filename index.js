d06faec4b0950f405de1d30d9d4b61fa66493668
Sergey <persalius@gmail.com>
2019-12-20 23:04:52 +0200

    edit cors

--- index.js
+++ index.js
@@ -14,6 +14,23 @@ const app = express();
 const server = http.createServer(app);
 const io = SocketIOServer(server);
 
+// Cors
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   next();
+//   app.options('*', (req, res) => {
+//     res.header(
+//       'Access-Control-Allow-Methods',
+//       'GET, PATCH, PUT, POST, DELETE, OPTIONS'
+//     );
+//     res.send();
+//   });
+// });
+
 // Open MongoDB connection
 mongoose.Promise = Promise;
 
