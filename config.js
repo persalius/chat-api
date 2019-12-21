e130997e49f7d4e89be0780d1f08099b1df17764
Sergey <persalius@gmail.com>
2019-12-21 13:29:16 +0200

    Edit db uri

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   JWT_SECRET: process.env.JWT_SECRET || 'persalius-secret',
   MONGODB_URI:
-    process.env.MONGODB_URI || 'mongodb://localhost/dogecodes-chat-app',
+    'mongodb://sergey1990:sergeysergey90@ds257668.mlab.com:57668/chat',
 };
